5bccd1715272373e8887c047c744924a9b8d68bb
hari charan <charangattamaneni@gmail.com>
2025-05-24 21:36:54 +0530

    ROBSHOP AWS

---
+++ roboshop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-08707020d4e3b5261"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z07011085YE8KA9PU3FY"
+DOMAIN_NAME="daws85s.site"
+
+for instance in ${INSTANCES[@]}
+do
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-08707020d4e3b5261 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
+   if [ $instance != frontend ]
+   then
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)  
+    fi
+    echo "$instance IP address: $IP"
+done
+
+# aws ec2 describe-instances \
+#     --instance-ids <instance-id> \
+#     --query "Reservations[0].Instances[0].{InstanceId:InstanceId, PublicIp:PublicIpAddress, PrivateIp:PrivateIpAddress}"
